382f766d6f6922a7a7926c4a533126813036c595
taga3s <bepozurapu274@gmail.com>
2025-12-03 20:28:49 +0900

    00043

---
+++ type-challenges/00043/index.ts
@@ -0,0 +1,7 @@
+type A = 'a' | 'b' | 'c';
+type B = 'a';
+type C = A extends B ? never : A; // not work: https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+
+type MyExclude<T, U> = T extends U ? never : T;
+
+type Result = MyExclude<A, B> // 'b' | 'c'
