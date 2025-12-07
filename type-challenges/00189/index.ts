543c0b903e7831ded23722330fad35514f527859
taga3s <bepozurapu274@gmail.com>
2025-12-07 22:22:30 +0900

    Update 00189

--- type-challenges/00189/index.ts
+++ type-challenges/00189/index.ts
@@ -1,5 +1,12 @@
 type ExampleType = Promise<string>;
+type ExampleThenableType = {
+  then: (onfulfilled: (arg: number) => any) => PromiseLike<any>;
+};
 
-type MyAwaited<T> = T extends Promise<infer R> ? R : never;
+type MyAwaited<T> = T extends PromiseLike<infer R>
+  ? R extends PromiseLike<any> ? MyAwaited<R>
+  : R
+  : never;
 
 type Result = MyAwaited<ExampleType>; // string
+type ResultThenable = MyAwaited<ExampleThenableType>; // number
