b437a92ffb691659bf320ff65880f49f027b0529
taga3s <bepozurapu274@gmail.com>
2025-12-07 22:21:30 +0900

    Update 00011

--- type-challenges/00011/index.ts
+++ type-challenges/00011/index.ts
@@ -1,8 +1,10 @@
 const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 
+// keyof any -> string | number | symbol
 // deno-lint-ignore no-explicit-any
-type TupleToObject<T extends readonly any[]> = {
-  [K in T[number]]: K;
+type Tuple = readonly (keyof any)[];
+type TupleToObject<T extends Tuple> = {
+  [P in T[number]]: P;
 };
 
 type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
